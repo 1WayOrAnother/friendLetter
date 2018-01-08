41f8ffb89535e4ffb5f4d813bb74a95211426cc2
Adam Calhoun and Paul Kotok <adam@>
2018-01-08 13:50:21 -0800

    add page link

---
+++ src/main/java/App.java
@@ -0,0 +1,41 @@
+import static spark.Spark.*;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+
+
+public class App {
+
+        public static void main(String[] args) {
+
+            staticFileLocation("/public");
+            get("/", (request, response) -> {
+                return new ModelAndView(new HashMap(), "hello.hbs");
+            }, new HandlebarsTemplateEngine());
+
+            get("/favorite_photos", (request, response) -> {
+                return new ModelAndView(new HashMap(), "favorite_photos.hbs");
+            }, new HandlebarsTemplateEngine());
+
+            get("/form", (request, response) -> {
+                Map<String, Object> model = new HashMap<String, Object>();
+                System.out.println(request.queryParams());
+                return new ModelAndView(model, "form.hbs");
+            }, new HandlebarsTemplateEngine());
+
+            get("/greeting_card", (request, response) -> {
+                Map<String, Object> model = new HashMap<String, Object>();
+                String recipient = request.queryParams("recipient");
+                String sender = request.queryParams("sender");
+                model.put("recipient", recipient);
+                model.put("sender", sender);
+                System.out.println(request.queryParams());
+                return new ModelAndView(model, "greeting_card.hbs");
+            }, new HandlebarsTemplateEngine());
+
+        }
+
+    }
+
